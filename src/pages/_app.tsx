386f20614d320e1094ad8499c9967a67d01b782c
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-03-20 15:41:15 -0300

    Testando a estilização usando o styled-component, criando uma tela simples

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
@@ -8,11 +8,11 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="" />
-        <link rel="apple-touch-icon" href="" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
-          content="A simple project starter to work with Typesript, React, NextJS and Styled Components"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
